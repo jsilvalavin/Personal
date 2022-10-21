939ac93b884b11e72422cc581801a1fdd47398f1
José Silva Lavin <jsilvalavin@uc.cl>
2022-10-21 18:10:03 -0300

    .

--- code.cpp
+++ code.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #include <fstream>
 ifstream file;
 
-double **matrix_generator(double filas, int columnas, int world_rank)
+double **matrix_generator(int filas, int columnas)
 {
     double **matrix = (double **)calloc(filas, sizeof(double *));
 
@@ -16,8 +16,44 @@ double **matrix_generator(double filas, int columnas, int world_rank)
     {
         matrix[i] = (double *)calloc(columnas, sizeof(double));
     }
-
     return matrix;
+}
+
+double* read_matrix(int nrows, int ncols, int firstrow, int localrows)
+{
+    double tmp;
+    ifstream file;
+    file.open("matrix.txt");
+
+    double* out;
+    out = new double [localrows * ncols];
+
+    for (int i = 0; i < (firstrow - 1) * ncols; i++)
+    {
+        file >> tmp;
+    }
+
+    for (int i = 0; i < localrows * ncols; i++)
+    {
+        file >> my_matrix[i];
+    }
+
+    file.close();
+
+    return out
+}
+
+void print_mat(double** matrix, int nrows, int ncols)
+{
+    printf("\n");
+    for (int i = 0; i < nrows; i++)
+    {
+        for (int j = 0; j < ncols; j++)
+        {
+            printf("%f ", matrix[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 void print_vec(double* vec, int k)
@@ -42,7 +78,7 @@ int main(){
 
     int iteraciones = 100000000;
 
-    // Abrir mpi
+    // Abrir mpi 
     MPI_Init(NULL, NULL);
     int world_size, world_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
@@ -70,9 +106,14 @@ int main(){
     for (int i = 0; i < ncols; i++)
     {
         localb[i] = 1;
-        cout << 1 << endl;
     }
     print_vec(localb, localrows);
+
+    // matriz local
+    double** localmat = matrix_generator(localrows, ncols);
+    
+    // valores de matriz
+    double* localvals = read_matrix(nrows, ncols, firstindex, localrows)
     
     // -----------------------------------------------
     MPI_Finalize();
